6ef9e18a9fcf34ebd838912979a770064b6cd86d
saurabhcses <157964303+saurabhcses@users.noreply.github.com>
2025-09-13 12:00:36 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,30 +1,32 @@
-//complete this code
 class Rectangle {
-	constructor(width,heigth){
-		this._width=width;
-		this._heigth=heigth;
-	}
-	get width(){
-		return this._width ;
-	
-	}
+  constructor(width, height) {
+    this._width = width;
+    this._height = height;
+  }
+
+  get width() {
+    return this._width;
+  }
+
   get height() {
     return this._height;
   }
-	getArea(){
-		return this._width*this._heigth;
-	}
+
+  getArea() {
+    return this._width * this._height;
+  }
 }
 
 class Square extends Rectangle {
-	super(side, side);
-	constructor(side){
-		this._side=side;
-		
-	}
-	getPerimeter(){
-		return 4*this._side;
-	}
+  constructor(side) {
+    
+    super(side, side);
+    this._side = side;
+  }
+
+  getPerimeter() {
+    return 4 * this._side;
+  }
 }
 
 // Do not change the code below this line
